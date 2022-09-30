1fdd5bb79e30ee624759c8dd7f004ee8609bc658
rybon35 <105024802+rybon35@users.noreply.github.com>
2022-09-29 21:23:19 -0500

    office hours

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -66,6 +66,7 @@ public class TechJobs {
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
+
             }
         }
     }
@@ -119,7 +120,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> jobField : job.entrySet()) {
+                    System.out.println(jobField.getKey() + ": " + jobField.getValue());
+                }
+                System.out.println("*****");
+            }
+        }
     }
+
 }
